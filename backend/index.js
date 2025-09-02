c0ba2707069ea5d46f8a3466c43a45711a8b70e1
Vrusha@170203 <vrushalihumbe@gmail.com>
2025-09-02 20:49:40 +0530

    create signup user api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+import authRoutes from "./routes/auth.route.js"
+
 dotenv.config()
 
 mongoose
@@ -14,6 +16,11 @@ mongoose
 
 const app = express()
 
+// for allowing json object in req body
+app.use(express.json())
+
 app.listen(5000, ()=>{
     console.log("server is running on port 5000!");
 })
+
+app.use("/api/auth", authRoutes)
