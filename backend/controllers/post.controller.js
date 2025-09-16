892c3d83254bbda2da0da09d5117110135a0e677
Vrusha@170203 <vrushalihumbe@gmail.com>
2025-09-16 21:49:21 +0530

    Create post api route

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,28 @@
+import Post from "../models/post.model.js"
+import { errorHandler } from "../utils/error.js"
+
+export const create = async(req,resizeBy, next) => {
+    if(!req.user.isAdmin) {
+        return next(errorHandler(403, "You are not authorized to create a post!"))
+    }
+
+    if(!req.body.title || !req.body.content) {
+        return next(errorHandler(400, "Please provide all the required field!"))
+    }
+
+    const slug = req.body.title.split(" ").join("-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "")
+
+    const newPost = new Post({
+        ...req.body,
+        slug,
+        userId: req.user.id,
+    })
+
+    try {
+        const savedPost = await newPost.save()
+
+        resizeBy.status(201).json(savedPost)
+    } catch (error) {
+        next(error)
+    }
+}
